2dee62accbf93ec0f6a7483a0924b17c278b4f1d
JIAJUNATBCIT <jiajuncai7@gmail.com>
2022-03-22 13:43:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+if (process.env.NODE_ENV !== "production")
+{
+  require("dotenv").config()
+}
+const express = require('express')
+const port = process.env.PORT
+const app = express()
+
+const events = require('./events')
+events.listenForEvents(app)
+
+app.listen(port, function () {
+  console.log(`Listening on ${port}`)
+})
