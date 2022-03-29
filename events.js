32784306d6b60f6e8fce3ba46ac14ba274953866
JIAJUNATBCIT <jiajuncai7@gmail.com>
2022-03-29 10:12:02 -0700

    add scripts

--- events.js
+++ events.js
@@ -53,7 +53,7 @@ async function respondToMention(event) {
   try {
     await web.chat.postMessage({
       channel: event.channel,
-      text: `Hello <@${event.user}>! I am Alice's virtual assistant, as you may know, Alice is on maternity leave till April.30, here're some questions that I may assist you with`,
+      text: `Hello <@${event.user}>! I am Alice's virtual assistant, as you may know, Alice is on maternity leave till April.30, here're some topics that I may assist you with`,
       attachments: [subjects]
     })
     //console.log('Message posted!')
